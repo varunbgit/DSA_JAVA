31fac794b25c0cedf8c0fb9b1a3f65570f915ed8
varunbgit <vbansalevents01@gmail.com>
2022-06-26 10:06:38 +0530

    Final Code on Cyclic sort

--- CyclicSort.java
+++ CyclicSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CyclicSort {       //Specifically only for the arrays that has values from range [ 1 to N ]
     public static void main(String[] args) {
-        int[] arr = {1};
+        int[] arr = {5,4,3,2,1};
         cyclicsort(arr);
         System.out.println(Arrays.toString(arr));
     }
